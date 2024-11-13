9bfa5914abcfe7ee1a03967f36fc1c05a0e569b1
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-12 20:05:55 -0500

    views

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const indexRouter = require('./routes/indexRouter');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
 app.use('/authors', authorRouter);
 app.use('/books', bookRouter);
 app.use('/', indexRouter);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
 
+const links = [
+  { href: '/', text: 'Home' },
+  { href: 'about', text: 'About' },
+];
+
+const users = ['Rose', 'Cake', 'Biff'];
+
 const indexRouter = Router();
 
-indexRouter.get('/', (req, res) =>
-  res.render('index', { message: 'EJS rocks!' }),
-);
+indexRouter.get('/', (req, res) => res.render('index', { links, users }));
 indexRouter.get('/about', (req, res) => res.send('About page'));
 indexRouter
   .route('/contact')
