1b330a3efabae0729289ded6ce074423df6207fd
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-27 14:32:59 -0500

    set for auth

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-require('dotenv/config');
-
 const path = require('node:path');
+const { Pool } = require('pg');
 const express = require('express');
-const indexRouter = require('./routes/indexRouter');
-const newRouter = require('./routes/newRouter');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const pool = new Pool({
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+});
 
 const app = express();
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const assetsPath = path.join(__dirname, 'public');
-app.use(express.static(assetsPath));
-app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/new', newRouter);
-app.use('/', indexRouter);
+app.get('/', (req, res) => res.render('index'));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
