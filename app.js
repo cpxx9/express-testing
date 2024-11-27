a68f25fc9c8f52a0101605b15fd36cd00d082107
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-27 16:10:49 -0500

    yeah buddy

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require('dotenv/config');
 const path = require('node:path');
+const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 const express = require('express');
 const session = require('express-session');
@@ -17,7 +19,79 @@ app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.render('index'));
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        'SELECT * FROM users WHERE username = $1',
+        [username],
+      );
+      const user = rows[0];
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username!' });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password!' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [
+      id,
+    ]);
+    const user = rows[0];
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/sign-up', (req, res) => res.render('sign-up-form'));
+app.post('/sign-up', async (req, res, next) => {
+  try {
+    await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect('/');
+  } catch (err) {
+    return next(err);
+  }
+});
+app.post(
+  '/log-in',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+  }),
+);
+
+app.get('/log-out', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
