aed05f70440dbac33b35163732f05cb27a54b24b
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-03 21:02:54 -0500

    session

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const pgSession = require('connect-pg-simple')(session);
 
-const pool = new Pool({
+const pgPool = new Pool({
   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
 });
 
@@ -15,14 +16,21 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    store: new pgSession({ pool: pgPool }),
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
-      const { rows } = await pool.query(
+      const { rows } = await pgPool.query(
         'SELECT * FROM users WHERE username = $1',
         [username],
       );
@@ -47,7 +55,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [
+    const { rows } = await pgPool.query('SELECT * FROM users WHERE id = $1', [
       id,
     ]);
     const user = rows[0];
@@ -71,10 +79,10 @@ app.post('/sign-up', async (req, res, next) => {
     if (err) {
       return next(err);
     }
-    await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [
-      req.body.username,
-      hashedPassword,
-    ]);
+    await pgPool.query(
+      'INSERT INTO users (username, password) VALUES ($1, $2)',
+      [req.body.username, hashedPassword],
+    );
     res.redirect('/');
   });
 });
