2921110af3ab7705ac8fc5f0f2582741a542ccfa
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-11 06:32:26 -0500

    rest and cors

--- routes/indexRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { logNames } = require('../controllers/indexController');
-
-const indexRouter = Router();
-
-indexRouter.get('/', logNames);
-
-module.exports = indexRouter;
--- routes/newRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const controller = require('../controllers/newController');
-
-const newRouter = Router();
-
-newRouter.route('/').get(controller.newUserGet).post(controller.newUserPost);
-
-module.exports = newRouter;
